e87b74ddb0817a8511e0f8720746e8de2796c9c7
GauClarisse <clarisse.gau@esme.fr>
2024-02-28 11:13:58 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,7 @@
-Update welcome message
+#include <stdio.h>
+
+int main() {
+    printf("Update welcome message\n"); // Ajoutez le message entre guillemets ou commentez correctement
+    return 0;
+}
+

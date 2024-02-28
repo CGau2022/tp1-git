0f642c211186ea787f897b01a73e62e1a545aebc
GauClarisse <clarisse.gau@esme.fr>
2024-02-28 11:21:19 +0100

    Revert "Update main.c"
    This reverts commit e87b74ddb0817a8511e0f8720746e8de2796c9c7.

--- main.c
+++ main.c
@@ -1,7 +1,1 @@
-#include <stdio.h>
-
-int main() {
-    printf("Update welcome message\n"); // Ajoutez le message entre guillemets ou commentez correctement
-    return 0;
-}
-
+Update welcome message
